a247614c6dff77469ae4497afb060e1c48bb4d7b
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-10-30 22:31:43 +0100

    feat: add commonRequestHeaders to `requestMethod` and `fetchMethod`

--- packages/shared/src/hooks/useRequestProtocol.ts
+++ packages/shared/src/hooks/useRequestProtocol.ts
@@ -1,19 +1,40 @@
 import { useMemo } from 'react';
-import request from 'graphql-request';
+import request, { RequestDocument, Variables } from 'graphql-request';
 import { useQueryClient } from 'react-query';
 import { RequestProtocol, REQUEST_PROTOCOL_KEY } from '../graphql/common';
+import { commonRequestHeaders } from '../lib/headers';
 
 export const useRequestProtocol = (): RequestProtocol => {
   const client = useQueryClient();
   const { requestMethod, fetchMethod, isCompanion } =
     client.getQueryData<RequestProtocol>(REQUEST_PROTOCOL_KEY) || {};
 
-  return useMemo(
-    () => ({
-      requestMethod: requestMethod || request,
-      fetchMethod: fetchMethod || fetch,
+  return useMemo(() => {
+    const requester = requestMethod || request;
+    const fetcher = fetchMethod || fetch;
+
+    return {
+      requestMethod: <T extends unknown, V extends Variables>(
+        url: string,
+        document: RequestDocument,
+        variables?: V,
+        requestHeaders?: HeadersInit,
+      ): Promise<T> =>
+        requester(url, document, variables, {
+          ...requestHeaders,
+          ...commonRequestHeaders,
+        }),
+
+      fetchMethod: (
+        input: RequestInfo | URL,
+        init?: RequestInit,
+      ): Promise<Response> =>
+        fetcher(input, {
+          ...init,
+          headers: { ...init?.headers, ...commonRequestHeaders },
+        }),
+
       isCompanion,
-    }),
-    [requestMethod, fetchMethod, isCompanion],
-  );
+    };
+  }, [requestMethod, fetchMethod, isCompanion]);
 };
